2a3c8a6c934d5cd727409cad8226d950d250b14d
naomi <naomi@nyaminyami.myfiosgateway.com>
2023-03-12 16:02:53 -0400

    Filled in some more code.

--- src/login.c
+++ src/login.c
@@ -1,6 +1,18 @@
 #include <gtk/gtk.h>
 
+int create_dialog(gchar *message)
+{
+	GtkWidget *dialog;
+	GtkWidget *login_input_field;
+	GtkWidget *pwd_input_field;
+	GtkWidget *login_label;
+	GtkWidget *pwd_label;
+	
+	return 0;
+}
+
 int create_login_window(GtkWidget *window, GtkApplication *app)
 {
-	// do stuff here
+	create_dialog("Log into MariaDB");
+	return 0;
 }
--- src/login.h
+++ src/login.h
@@ -3,6 +3,7 @@
 #ifndef LOGIN_H_ 
 #define LOGIN_H_
 
+int create_dialog(gchar *message);
 int create_login_window(GtkWidget *window, GtkApplication *app);
 
 #endif
