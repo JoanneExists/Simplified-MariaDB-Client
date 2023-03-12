3d306bd437773ee809be155830df19dd1ffc3cf7
naomi <naomi@nyaminyami.myfiosgateway.com>
2023-03-12 15:46:47 -0400

    Fixed activate def in header file.

--- src/main_window.h
+++ src/main_window.h
@@ -3,7 +3,7 @@
 #ifndef MAIN_WINDOW_H_ 
 #define MAIN_WINDOW_H_
 
-int act(GtkApplication *app, gpointer user_data);
+int activate(GtkApplication *app, gpointer user_data);
 int create_main_window();
 
 #endif
